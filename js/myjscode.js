435a34bd8f6eb1d6c97cb1a47e729645be23d7cb
miriamhabari <miriamhabar@gmail.com>
2017-01-05 00:15:25 +0200

    basic template

---
+++ js/myjscode.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {    
+  
+  // execute the slideShow, set 4 seconds (4000) for each image
+  console.log("new js file",location);
+  if(location=="file:///C:/Users/miriam/Desktop/itremp/website/template/photoartwork2_reverse/portfolio_one.html"){
+  	location="file:///C:/Users/miriam/Desktop/itremp/website/template/photoartwork2_reverse/slider-view.html";
+  }
+	//location="file:///C:/Users/miriam/Desktop/itremp/website/template/photoartwork2_reverse/slider-view.html";
+});
